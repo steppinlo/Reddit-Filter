f27ef8971349257db5eb46edb32d6c3739cfaf9e
Ben Cheng & Angel Baek & Stephanie Lo & Tracy Teague <pair+bcheng90.gellieb.steppinlo.tracyteague@devbootcamp.com>
2015-04-15 15:22:52 -0400

    adding games

--- server/seeds.js
+++ server/seeds.js
@@ -1,11 +1,35 @@
 Meteor.startup(function () {
   if (Teams.find().count() === 0) {
     [
-      {name: "BenisaTen"},
-      {name: "TracyissoCracy"},
-      {name: "Angela"}
+      {
+        name: "BenisaTen",
+        gameIds: []
+      },
+      {
+        name: "TracyissoCracy",
+        gameIds: []
+      },
+      {
+        name: "Angela",
+        gameIds: []
+      }
     ].forEach(function(team){
       Teams.insert(team);
     });
+
+        // Create a game
+    var team1 = Teams.find().fetch()[0];
+    var team2 = Teams.find().fetch()[1];
+
+    var game = {
+      completed: false,
+      createdAt: new Date(),
+      teams: [
+        {name: team1.name, _id: team1._id, score: 0},
+        {name: team2.name, _id: team2._id, score: 0}
+      ]
+    };
+
+    Games.insert(game);
   }
 });
